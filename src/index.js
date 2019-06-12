0fe32d95f88801571383c0dcf0bbc49ce4afd975
= <=>
2019-06-11 22:24:41 -0300

    Iniciar estrutura da api e rota de likes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb+srv://admin:123@cluster0-jncdk.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+
+})
+
+app.use(require('./routes'));
+app.listen(3333);
+
